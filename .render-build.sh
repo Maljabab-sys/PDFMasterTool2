01495d2d9f46c3c1bc761fb34e72fafa49bb1d67
Maljabab-sys <mhanna.aj@gmail.com>
2025-06-16 06:12:36 +0300

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -3,6 +3,11 @@
 # 1) Install Python dependencies
 pip install -r requirements.txt
 
-# 2) Install & build React frontend
+# 2) Build React frontend
+cd frontend
 npm install
 npm run build
+
+# 3) Move React's build/ up one level
+rm -rf ../build
+mv build ../build
